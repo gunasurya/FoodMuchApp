a54657da12df079eabfc5fe81b9dd47d37d30f36
gunasurya <119664985+gunasurya@users.noreply.github.com>
2024-05-06 23:45:44 +0530

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+let links = document.getElementsByClassName('nav-link');
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function() {
+        document.querySelectorAll('.nav-link').forEach(link => {
+            link.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
+});
+
